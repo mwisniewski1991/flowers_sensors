e667fd85e1ab3ae43fdc46a9ca54bbe094d2448e
mwisniewski <mw.data.web@gmail.com>
2023-07-13 19:36:08 +0200

    Update pico code

--- pico/app.py
+++ pico/app.py
@@ -1,26 +1,61 @@
-from machine import Pin, ADC
+import network
+import gc
+import urequests
 import utime as time
+from machine import Pin, ADC
 from dht import DHT11
+from config import NETWORK_NAME, NETWORK_PASS, API_URL
 
-LED_PIN_NUMBER = Pin(14, Pin.OUT)
+gc.enable()
+gc.collect()
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(NETWORK_NAME, NETWORK_PASS)
+
+# Handle connection error
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print('ip = ' + status[0] )
+    print('-------------------------------')
+
+
+LED_PIN_NUMBER = 14
 DHT_PIN_NUMBER = 28
+
+led = Pin(LED_PIN_NUMBER, Pin.OUT)
 adc = ADC(26)
 conversion_factor = 100 / (65535)
 
 while True:
-    time.sleep(2.5)
+    time.sleep(5)
 
-    LED_PIN_NUMBER.toggle()
-
-    pin = Pin(DHT_PIN_NUMBER, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
-
+    led.toggle()
     moisture = 130 - (adc.read_u16() * conversion_factor)
-    temperature = sensor.temperature
-    humidity = sensor.humidity
 
-    print("-------------------------------")
-    print("Moisture: {}".format(moisture))
-    print("Temperature: {}".format(temperature))
-    print("Humidity: {}".format(humidity))
-    
+    try:
+        pin = Pin(DHT_PIN_NUMBER, Pin.OUT, Pin.PULL_DOWN)
+        sensor = DHT11(pin)
+        temperature = sensor.temperature
+        humidity = sensor.humidity
+
+    except:
+        print("ERROR FOR SENSOR")
+        temperature = 0
+        humidity = 0
+        
+    # url = "http://192.168.18.15:81/sensor?id=1234&temeprature=" + str(temperature) + "&humidity=" + str(humidity) + "&moisture=" + str(moisture)
+    url = API_URL + "?id=1234&temeprature=" + str(temperature) + "&humidity=" + str(humidity) + "&moisture=" + str(moisture)
+
+
+    # print("-------------------------------")
+    # print("Moisture: {}".format(moisture))
+    # print("Temperature: {}".format(temperature))
+    # print("Humidity: {}".format(humidity))
+
+    r = urequests.get(url)
+    print(r.json())
+    r.close()
